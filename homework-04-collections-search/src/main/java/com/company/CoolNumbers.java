7dedfd0dc6fbf07a5f8cfac34c3bd3a7b51cfe5b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-18 16:20:11 +0300

    add new class

---
+++ homework-04-collections-search/src/main/java/com/company/CoolNumbers.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
+public class CoolNumbers {
+
+    public static List<String> generateCoolNumbers() {
+        return List.of();
+    }
+
+    public static boolean bruteForceSearchInList(List<String> list, String number) {
+        return false;
+    }
+
+    public static boolean binarySearchInList(List<String> sortedList, String number) {
+        return false;
+    }
+
+    public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        return false;
+    }
+
+    public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        return false;
+    }
+}
