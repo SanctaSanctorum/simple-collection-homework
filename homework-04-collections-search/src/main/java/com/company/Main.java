e0be1965fee8891be7faec7c64e399d17ad775ec
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-18 16:20:27 +0300

    add todo

--- homework-04-collections-search/src/main/java/com/company/Main.java
+++ homework-04-collections-search/src/main/java/com/company/Main.java
@@ -1,6 +1,14 @@
 package com.company;
 
 public class Main {
+
+    /*
+    TODO:
+     - реализовать методы класса CoolNumbers
+     - посчитать время поиска вводимого номера в консоль в каждой из структуры данных
+     - проанализировать полученные данные
+     */
+
     public static void main(String[] args) {
 
     }
